99f7ebd81756f3dc83056eaca76075b545ff7713
U-Zyn Chua <chua@uzyn.com>
2018-12-07 23:32:18 +0800

    Correct allocation

--- contracts/SPO.sol
+++ contracts/SPO.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.4.24;
 
-import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
-import "openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
+import 'openzeppelin-solidity/contracts/token/ERC20/ERC20.sol';
+import 'openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol';
 
 /**
  * @title Sparrow Options
@@ -15,7 +15,16 @@ contract SPO is ERC20, ERC20Detailed {
     /**
      * @dev Constructor that gives msg.sender all of existing tokens.
      */
-    constructor () public ERC20Detailed("Sparrow Options", "SPO", 18) {
-        _mint(msg.sender, TOTAL_SUPPLY);
+    constructor () public ERC20Detailed('Sparrow Options', 'SPO', 18) {
+        _mint(0xCF44ccB8f27aEA1abb054d4Ae89AA00CFbda3603, 376000000 * (10 ** 18)); // ICO
+        _mint(0xEd6D7d1Ffb258bB82A6B5Cf2C2377DCAA6A0b533, 57000000 * (10 ** 18)); // Bonus
+        _mint(0x1fcE49D845036a54E348fe10Aa6f541DEb48547a, 70000000 * (10 ** 18)); // Advisors
+        _mint(0xEf235b3CaAA268C4Db6215879a436Af751A0F729, 140000000 * (10 ** 18)); // Founders
+        _mint(0x49035f3a38644d805C8AEdA5eB5fAA5DB54A5fA0, 70000000 * (10 ** 18)); // Employees
+        _mint(0xdaBF505B241695788780b12515066746fDD65592, 337000000 * (10 ** 18)); // Reserves Pool
+        _mint(0xEf13476b380C31CA6B1FB5C49d5CfCedcdB90b69, 140000000 * (10 ** 18)); // Marketing
+        _mint(0xe998a139D865883C8964d34048a9acb75Eb314Ba, 210000000 * (10 ** 18)); // Strategic Partnerships
+
+        assert(totalSupply() == TOTAL_SUPPLY); // Ensure that exactly all total supply has been allocated
     }
 }
--- test/contracts/SPO.test.js
+++ test/contracts/SPO.test.js
@@ -10,7 +10,16 @@ contract('SPO smart contract', () => {
     spo = await SPO.deployed();
   });
 
-  it('has the correct total number of token supply', async () => {
+  it('has the correct total number of token supply and correct allocations', async () => {
     assert.equal(await spo.totalSupply(), 1.4 * 1e9 * 1e18);
+
+    assert.equal(await spo.balanceOf('0xCF44ccB8f27aEA1abb054d4Ae89AA00CFbda3603'), 376000000 * 1e18); // ICO
+    assert.equal(await spo.balanceOf('0xEd6D7d1Ffb258bB82A6B5Cf2C2377DCAA6A0b533'), 57000000 * 1e18); // Bonus
+    assert.equal(await spo.balanceOf('0x1fcE49D845036a54E348fe10Aa6f541DEb48547a'), 70000000 * 1e18); // Advisors
+    assert.equal(await spo.balanceOf('0xEf235b3CaAA268C4Db6215879a436Af751A0F729'), 140000000 * 1e18); // Founders
+    assert.equal(await spo.balanceOf('0x49035f3a38644d805C8AEdA5eB5fAA5DB54A5fA0'), 70000000 * 1e18); // Employees
+    assert.equal(await spo.balanceOf('0xdaBF505B241695788780b12515066746fDD65592'), 337000000 * 1e18); // Reserves Pool
+    assert.equal(await spo.balanceOf('0xEf13476b380C31CA6B1FB5C49d5CfCedcdB90b69'), 140000000 * 1e18); // Marketing
+    assert.equal(await spo.balanceOf('0xe998a139D865883C8964d34048a9acb75Eb314Ba'), 210000000 * 1e18); // Strategic Partnerships
   });
 });
